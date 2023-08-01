f5fb7442c812e2a1276a0a7eb191eb129e54168a
Hossein Fatemi <hfatemi1997@gmail.com>
2023-08-01 19:10:24 +0330

    End E11

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use strict";
+// Class
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting);
---
+++ app.ts
@@ -0,0 +1,12 @@
+// Class
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department("Accounting");
+
+console.log(accounting);
