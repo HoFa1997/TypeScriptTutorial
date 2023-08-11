f690fa9a06eb27bb05793200a5d1c3adced3d254
Hossein Fatemi <hfatemi1997@gmail.com>
2023-08-11 14:00:21 +0330

    End 23

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 "use strict";
-let user1;
-user1 = {
-    name: "Ali",
-    age: 20,
+class Person {
+    constructor(name, age, companySize) {
+        this.name = name;
+        this.age = age;
+        this.companySize = companySize;
+    }
     hello(name) {
-        console.log("Hello " + name);
-    },
-};
-user1.hello("Ali");
+        console.log(name);
+    }
+}
+let user1;
+user1 = new Person("Ali", 24, 100);
+console.log(user1);
+user1.hello("hossein");
+// user1 = {
+//   name: "Ali",
+//   age: 20,
+//   hello(name: string) {
+//     console.log("Hello " + name);
+//   },
+// };
+// user1.hello("Ali");
--- app.ts
+++ app.ts
@@ -4,14 +4,36 @@ interface IPerson {
   hello(name: string): void;
 }
 
+interface ICompany {
+  companySize: number;
+}
+
+class Person implements IPerson, ICompany {
+  constructor(
+    public name: string,
+    public age: number,
+    public companySize: number
+  ) {}
+
+  hello(name: string): void {
+    console.log(name);
+  }
+}
+
 let user1: IPerson;
 
-user1 = {
-  name: "Ali",
-  age: 20,
-  hello(name: string) {
-    console.log("Hello " + name);
-  },
-};
+user1 = new Person("Ali", 24, 100);
 
-user1.hello("Ali");
+console.log(user1);
+
+user1.hello("hossein");
+
+// user1 = {
+//   name: "Ali",
+//   age: 20,
+//   hello(name: string) {
+//     console.log("Hello " + name);
+//   },
+// };
+
+// user1.hello("Ali");
