6b2ecce1dad37bc57f0a9d01334761d2a8598c6b
Hossein Fatemi <hfatemi1997@gmail.com>
2023-09-25 01:16:11 +0330

    delete node module

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const e1 = {
     permission: ["list-of-user"],
     startDate: new Date(),
 };
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
--- app.ts
+++ app.ts
@@ -21,3 +21,27 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+type Animal = Bird | Horse;
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
