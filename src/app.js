4c5a2c6997167a9e83805130e4a1875b23bd4a7d
Hossein Fatemi <hfatemi1997@gmail.com>
2023-09-25 21:12:36 +0330

    add new files

--- src/app.js
+++ src/app.js
@@ -16,3 +16,8 @@ function moveAnimal(animal) {
     console.log("Moving at speed: " + speed);
 }
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
+const userInputElement = document.getElementById("user-input");
+if (userInputElement) {
+    userInputElement.value = "Hi there";
+}
--- src/app.ts
+++ src/app.ts
@@ -45,3 +45,10 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there";
+}
