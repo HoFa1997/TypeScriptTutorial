4acf11ace88b2e6a24752fc6778bb6cfdc4047c8
Hossein Fatemi <hfatemi1997@gmail.com>
2023-09-29 17:55:42 +0330

    end e36

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// Start
+
+const names: Array<string> = ["A", "B"]; // string[]
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("this is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  //   data.split(" ");
+});
