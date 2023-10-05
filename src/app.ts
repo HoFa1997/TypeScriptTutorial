d46ea83b83a9d55b246b6c5f49a1c241d2983847
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-05 14:46:57 +0330

    decorators

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
 // START
+
+function Logger(constructor: Function) {
+  console.log("Logging...");
+  console.log(constructor);
+}
+@Logger
+class Person {
+  name = "A";
+
+  constructor() {
+    console.log("Creating person Object ...");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
