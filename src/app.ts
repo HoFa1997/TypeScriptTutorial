98cfce5af31b2fd69e9927fc54dee62c6eb949af
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-05 17:14:20 +0330

    end Diving into Property Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,63 @@
 // START
 
-function Logger(logString: string) {
-  console.log("Logger Factory");
+// function Logger(logString: string) {
+//   console.log("Logger Factory");
 
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
+
+// function WithTemplate(template: string, hookId: string) {
+//   console.log("WithTemplate Factory");
+//   return function (constructor: any) {
+//     const hookEl = document.getElementById(hookId);
+//     const p = new constructor();
+//     if (hookEl) {
+//       hookEl.innerHTML = template;
+//       hookEl.querySelector("h1")!.textContent = p.name;
+//     }
+//   };
+// }
+
+// @Logger("Logging Person")
+// @WithTemplate("<h1>My Person Obj</h1>", "app")
+// class Person {
+//   name = "Hossein";
+//   constructor() {
+//     console.log("Creating person Object ...");
+//   }
+// }
+// const pers = new Person();
+// console.log(pers);
+
+// ------
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("property decorator");
+  console.log(target, propertyName);
 }
 
-function WithTemplate(template: string, hookId: string) {
-  console.log("WithTemplate Factory");
+class Product {
+  @Log
+  title: string;
+  _price: number;
 
-  return function (constructor: any) {
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid Price");
     }
-  };
-}
+  }
 
-@Logger("Logging Person")
-@WithTemplate("<h1>My Person Obj</h1>", "app")
-class Person {
-  name = "Hossein";
-  constructor() {
-    console.log("Creating person Object ...");
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this.price * (1 + tax);
   }
 }
-const pers = new Person();
-console.log(pers);
