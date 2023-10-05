44cbe898442e042f1c130d947e1c6e26510924b1
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-05 16:18:07 +0330

    End Building More Useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,22 @@ function Logger(logString: string) {
     console.log(constructor);
   };
 }
-@Logger("Logging Person")
-class Person {
-  name = "A";
 
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Logging Person")
+@WithTemplate("<h1>My Person Obj</h1>", "app")
+class Person {
+  name = "Hossein";
   constructor() {
     console.log("Creating person Object ...");
   }
