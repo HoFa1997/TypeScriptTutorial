b9f2f2d269d34faa9bd9e424fd68cd2904eb52a2
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-09 18:02:27 +0330

    Returning (and changing) a Class in a Class Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,95 @@
 // START
 
-// function Logger(logString: string) {
-//   console.log("Logger Factory");
+function Logger(logString: string) {
+  console.log("Logger Factory");
 
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
 
-// function WithTemplate(template: string, hookId: string) {
-//   console.log("WithTemplate Factory");
-//   return function (constructor: any) {
-//     const hookEl = document.getElementById(hookId);
-//     const p = new constructor();
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector("h1")!.textContent = p.name;
-//     }
-//   };
-// }
+function WithTemplate(template: string, hookId: string) {
+  console.log("WithTemplate Factory");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering Template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
 // @Logger("Logging Person")
-// @WithTemplate("<h1>My Person Obj</h1>", "app")
-// class Person {
-//   name = "Hossein";
-//   constructor() {
-//     console.log("Creating person Object ...");
-//   }
-// }
-// const pers = new Person();
-// console.log(pers);
+@WithTemplate("<h1>My Person Obj</h1>", "app")
+class Person {
+  name = "Hossein";
+  constructor() {
+    console.log("Creating person Object ...");
+  }
+}
+const pers = new Person();
+console.log(pers);
 
 // ------
 
-function Log(target: any, propertyName: string | Symbol) {
-  console.log("property decorator");
-  console.log(target, propertyName);
-}
+// function Log(target: any, propertyName: string | Symbol) {
+//   console.log("property decorator");
+//   console.log(target, propertyName);
+// }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Accessor decorator!");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("Accessor decorator!");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
 
-function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Method decorator!");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+// function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("Method decorator!");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
 
-function Log4(target: any, name: string, position: number) {
-  console.log("Parameter decorator!");
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
+// function Log4(target: any, name: string, position: number) {
+//   console.log("Parameter decorator!");
+//   console.log(target);
+//   console.log(name);
+//   console.log(position);
+// }
 
-class Product {
-  @Log
-  title: string;
-  _price: number;
+// class Product {
+//   @Log
+//   title: string;
+//   _price: number;
 
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error("Invalid Price");
-    }
-  }
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Invalid Price");
+//     }
+//   }
 
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
 
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this.price * (1 + tax);
-  }
-}
-const p1 = new Product("Book", 19);
-const p2 = new Product("Book 2", 29);
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this.price * (1 + tax);
+//   }
+// }
+// const p1 = new Product("Book", 19);
+// const p2 = new Product("Book 2", 29);
